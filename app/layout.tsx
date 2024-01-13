98cd0da98aaa77db8df0683d753e41d17c0885b6
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-01-13 06:16:53 +0500

    added page speed tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
@@ -66,6 +67,7 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
