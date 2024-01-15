533f20a2f1b9db2041058891f42b9d29d4c4e2fc
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-01-15 07:14:41 +0500

    added transition for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,15 @@
-'use client';
+"use client";
 import { Container } from "./container";
 import React from "react";
-
+import { motion } from "framer-motion";
 export const Footer = () => {
   return (
-    <footer className="py-8">
+    <motion.footer
+      className="py-8"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.75 }}
+    >
       <Container className="px-5 text-center flex justify-center">
         <div className="overflow-hidden rounded-xl px-3 sm:px-5 py-2 relative">
           <div className="backdrop-blur-md bg-lightBorderColor dark:bg-darkBorderColor opacity-25 absolute top-0 left-0 right-0 bottom-0 w-full h-full z-10" />
@@ -14,6 +19,6 @@ export const Footer = () => {
           </p>
         </div>
       </Container>
-    </footer>
+    </motion.footer>
   );
 };
