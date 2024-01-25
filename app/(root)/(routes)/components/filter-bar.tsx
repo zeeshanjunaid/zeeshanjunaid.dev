53bb03db23f22534a21e9a8d09a634d7ecbdbd88
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-01-25 10:43:47 +0500

    removed unused code from filter bar component

--- app/(root)/(routes)/components/filter-bar.tsx
+++ app/(root)/(routes)/components/filter-bar.tsx
@@ -20,28 +20,6 @@ import { BlurBG } from "@/components/blur-bg";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
 interface FilterBarProps {
   skills: string[];
   skillValue: string;
