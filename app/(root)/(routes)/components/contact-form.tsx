548883285435cc46ad5f2e4800422233e842f76c
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-02-07 20:18:51 +0500

    small contact form update

--- app/(root)/(routes)/components/contact-form.tsx
+++ app/(root)/(routes)/components/contact-form.tsx
@@ -25,12 +25,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 const formSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   email: z.string().email("Invalid email address"),
-  phone: z
-    .string()
-    .refine((val) => /^\+\d{1,3}\d{10}$/.test(val), {
-      message: "Invalid phone number",
-    })
-    .optional(),
+  phone: z.string().refine((val) => /^\+\d{1,3}\d{10}$/.test(val), {
+    message: "Invalid phone number",
+  }),
   referral: z.string().optional(),
   message: z.string().nonempty({ message: "Message is required" }),
 });
