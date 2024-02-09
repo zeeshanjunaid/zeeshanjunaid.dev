98bd426360764f665f3d841658b7358c01bcf09d
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-02-10 02:15:02 +0500

    spacing fixed for contact info

--- app/(root)/(routes)/contact/page.tsx
+++ app/(root)/(routes)/contact/page.tsx
@@ -27,34 +27,34 @@ const ContactPage = () => {
               today to bring your digital dreams to life. Let&apos;s build
               experiences that resonate and captivate your audience.
             </p>
-            <div className="flex flex-col md:flex-row gap-5  md:gap-8 md:flex-wrap lg:justify-between md:items-center mt-2.5 md:mt-8">
-              <div className="flex items-center justify-start gap-x-1.5">
-                <Mail size={18} />
-                <a
-                  href="mailto:hello@zeeshanjunaid.dev"
-                  className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
-                >
-                  hello[at]zeeshanjunaid[dot]dev
-                </a>
-              </div>
-              <div className="flex items-center justify-start gap-x-1.5">
-                <Phone size={18} />
-                <a
-                  href="tel:+923408563525"
-                  className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
-                >
-                  +92 340 856 3525
-                </a>
-              </div>
-              <div className="flex items-center justify-start gap-x-1.5">
-                <BsWhatsapp size={18} />
-                <a
-                  href="whatsapp://send?phone=923408563525"
-                  className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
-                >
-                  +92 340 856 3525
-                </a>
-              </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-5  md:gap-8 md:flex-wrap lg:justify-between md:items-center mt-6 md:mt-8">
+            <div className="flex items-center justify-start gap-x-1.5">
+              <Mail size={18} />
+              <a
+                href="mailto:hello@zeeshanjunaid.dev"
+                className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
+              >
+                hello[at]zeeshanjunaid[dot]dev
+              </a>
+            </div>
+            <div className="flex items-center justify-start gap-x-1.5">
+              <Phone size={18} />
+              <a
+                href="tel:+923408563525"
+                className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
+              >
+                +92 340 856 3525
+              </a>
+            </div>
+            <div className="flex items-center justify-start gap-x-1.5">
+              <BsWhatsapp size={18} />
+              <a
+                href="whatsapp://send?phone=923408563525"
+                className="text-[16px] md:text-[18px] lg:text-5 font-switzer text-dark dark:text-light underline underline-purple underline-offset-4 hover:text-dark/90 dark:hover:text-light/90 transition-color duration-200"
+              >
+                +92 340 856 3525
+              </a>
             </div>
           </div>
         </Container>
