e9969b9eee8b2c2c25232bb8932da55668404685
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2024-03-01 10:39:48 +0500

    service pages updated

---
+++ app/(root)/(routes)/components/back-home-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronsLeft } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const BackHomeButton = () => {
+  return (
+    <Link href="/">
+      <Button
+        className="mt-8 space-x-1 w-max rounded-xl text-dark dark:text-light uppercase text-[12px] md:text-[14px]]"
+        variant="purple"
+      >
+        <ChevronsLeft className="h-4 w-4" />
+        Go Back Home
+      </Button>
+    </Link>
+  );
+};
+
+export default BackHomeButton;
--- app/(root)/(routes)/services/wordpress-development/page.tsx
+++ app/(root)/(routes)/services/wordpress-development/page.tsx
@@ -1,7 +1,16 @@
+import BackHomeButton from "../../components/back-home-button";
+import { Container } from "@/components/container";
 import React from "react";
 
 const WordpressDevelopmentPage = () => {
-  return <div>WordpressDevelopmentPage</div>;
+  return (
+    <Container className="mt-5 py-8 px-5 md:px-7 lg:px-0">
+      <h2 className="text-dark dark:text-light font-ao font-bold text-5xl">
+        This page is coming soon
+      </h2>
+      <BackHomeButton />
+    </Container>
+  );
 };
 
 export default WordpressDevelopmentPage;
