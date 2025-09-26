fb81de2b4707c72304b98d3383b539bb82c36887
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2025-09-26 16:32:08 +0500

    fixed work filter z-index

--- app/(root)/(routes)/components/work-wrapper.tsx
+++ app/(root)/(routes)/components/work-wrapper.tsx
@@ -69,7 +69,7 @@ const WorkWrapper = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
-        className="relative overflow-x-hidden"
+        className="relative"
       >
         {/* Background Elements */}
         <div className="absolute inset-0 -z-10">
